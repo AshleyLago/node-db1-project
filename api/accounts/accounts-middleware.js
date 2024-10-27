30e1da92258dd5d1ffe4ed3188678076f415be4c
Ashley Lago <ashl3ylag0@gmail.com>
2024-10-27 01:52:01 -0400

    Completed project

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -10,20 +10,25 @@ const db = require('../../data/db-config')
 // Note: you can either write "manual" validation logic
 // or use the Yup library (not currently installed)
 exports.checkAccountPayload = (req, res, next) => {
-  const {name, budget} = req.body
-  if (name === undefined || budget === undefined) next({ status:400, message: "name and budget are required" })
-  if (name.trim() < 3 || name.trim() > 100) next({ status:400, message: "name of account must be between 3 and 100" })
-  if (isNaN(Number(budget))) next({ status:400, message: "budget of account must be a number"})
-  if (budget < 0 || budget > 1000000) next({ status:400, message: "budget of account is too large or too small"})
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined) {
+    next({ status:400, message: "name and budget are required" })
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    next({ status:400, message: "name of account must be between 3 and 100" })
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    next({ status:400, message: "budget of account must be a number"})
+  } else if (budget < 0 || budget > 1000000) {
+    next({ status:400, message: "budget of account is too large or too small"})
+  }
   next()
 }
 
-// Returns a status 404 with a { message: "account not found" } if req.params.id does not exist in the database
+// Returns a status 400 with a { message: "that name is taken" } if the trimmed req.body.name already exists in the database 
 exports.checkAccountNameUnique = async (req, res, next) => {
   try {
     const existAccount = await db('accounts').where('name', req.body.name.trim()).first()
     if (existAccount) {
-      next({ status:404, message: "account not found" })
+      next({ status:400, message: "that name is taken" })
     } else {
       next()
     }
@@ -32,7 +37,7 @@ exports.checkAccountNameUnique = async (req, res, next) => {
   }
 }
 
-// Returns a status 400 with a { message: "that name is taken" } if the trimmed req.body.name already exists in the database 
+// Returns a status 404 with a { message: "account not found" } if req.params.id does not exist in the database
 exports.checkAccountId = async(req, res, next) => {
   try {
     const account = await Accounts.getById(req.params.id)
@@ -40,7 +45,7 @@ exports.checkAccountId = async(req, res, next) => {
       req.account = account
       next()
     } else {
-      next({ status:400, message: "that name is taken" })
+      next({ status:404, message: "account not found" })
     }
   } catch (err) {
     next(err)
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,101 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model')
 
+const {checkAccountId, checkAccountNameUnique, checkAccountPayload} = require('./accounts-middleware')
+
+// Returns an array of accounts (or an empty array if there aren't any).
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.getAll()
+    .then(acc => {
+      res.status(200).json(acc)
+    })
+    .catch(next)
 })
+/* Alternate:
+  router.get('/', async (req, res, next) => {
+    try {
+      const acc = await Accounts.getAll()
+      res.status(200).json(acc)
+    } catch (err) {
+      next(err)
+    }
+  })
+*/
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+// Returns an account by the given id.
+router.get('/:id', checkAccountId, (req, res) => {
+  res.status(200).json(req.account)
 })
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+/* Alternate:
+router.get('/:id', checkAccountId, async(req, res, next) => {
+  try {
+      const account = await Accounts.getById(req.params.id)
+      res.json(account)
+    } catch (err) {
+      next(err)
+    }
 })
+*/
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+// Returns the created account. Leading or trailing whitespace on budget name should be trimmed before saving to db.
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
+  Accounts.create({ name: req.body.name.trim(), budget: req.body.budget })
+    .then(acc => {
+      res.status(201).json(acc);
+    })
+    .catch(next);
+})
+/* Alternate:
+  router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
+    try {
+      const newAcc = await Accounts.create({ name: req.body.name.trim(), budget: req.body.budget })
+      res.status(201).json(newAcc)
+    } catch (err) {
+      next(err)
+    }
+  })
+*/
+
+// Returns the updated account. Leading or trailing whitespace on budget name should be trimmed before saving to db.
+router.put('/:id', checkAccountId, checkAccountPayload, (req, res, next) => {
+  Accounts.updateById(req.params.id, req.body)
+    .then(upAcc => {
+      res.status(200).json(upAcc)
+    })
+    .catch(next)
 });
+/* Alternate:
+  router.put('/:id', checkAccountId, checkAccountPayload, async(req, res, next) => {
+    try {
+      const updated = await Accounts.updateById(req.params.id, req.body)
+      res.json(updated)
+    } catch(err) {
+      next(err)
+    }
+  });
+*/
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+// Returns the deleted account.
+router.delete('/:id', checkAccountId, (req, res, next) => {
+  Accounts.deleteById(req.params.id)
+    .then(() => {
+      res.json(req.user)
+    })
+    .catch(next)
 })
+/* Alternate:
+  router.delete('/:id', checkAccountId, async(req, res, next) => {
+    try {
+      await Accounts.deleteById(req.params.id)
+      res.json(req.user)
+    } catch (err) {
+      next(err)
+    }
+  })
+*/
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({ message: err.message })
 })
 
 module.exports = router;
